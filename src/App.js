290fee4e4b19717e108bd04668346bc38a5cd7bb
Kayden Clark <kaydenclark725@gmail.com>
2020-01-03 06:44:19 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './kcLogo.svg';
 import './App.css';
 
 function App() {
+  
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          This is Kayden's Portfolio site
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
+          href="#"
+          // target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Kayden's App
         </a>
       </header>
     </div>
