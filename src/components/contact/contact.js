a231a25681ce8b443c0a74015c24fdf6fb7ef30e
Kayden Clark <kaydenclark725@gmail.com>
2020-01-05 23:25:29 -0700

    added contacts page

--- src/components/contact/contact.js
+++ src/components/contact/contact.js
@@ -1,13 +1,18 @@
 import React from 'react'
+import './contact.css'
 
 export default class Contact extends React.Component {
 
     render(){
         return(
-            <div className= "Home"  >
-                <h1 id= "name" >Kayden Clark</h1>
-                <div className= "mainBody" >
-                    <p>I have studied programming for nearly 5yrs now, first in High School in programming class where I studied the fundamentals of programming using Python. My senior year of high school I took an intro to web development where I started to learn how to build static front-end websites using HTML 5 and CSS 3. At Helio Training I jumped head-first into web development with their full stack immersive course. There I learned JavaScript and how to use frameworks such as react and build backend API's with Express. I especially enjoy the challenge of backend development. I pride myself in my constructive problem-solving skills which I feel programming really eventuates. I am also proficient in Adobe programs such as photoshop and illustrator and have taken design courses in both.  </p>
+            <div className= "ContactInfo" >
+                <h1 id= "header">Contact Information</h1>
+                <div className= "contactBody">
+                    <ul className= "contactList">
+                        <li> Email: kaydenclark725@gmail.com</li>
+                        <li> Phone: (801) 448-5335</li>
+                    </ul>
+                    <p id= "endCard">Contact me anytime by call, text, or email</p>
                 </div>
             </div>
         )
